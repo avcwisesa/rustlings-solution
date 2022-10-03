b1d5dc947723ae0c0595ff01eac70ef71c55e165
Avicenna Wisesa <avcwisesa@gmail.com>
2022-10-03 23:41:18 +0800

    Completed quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    const PRICE: i32 = 2;
+    const DISCOUNTED_PRICE: i32 = 1;
+
+    if amount > 40 {
+        amount * DISCOUNTED_PRICE
+    } else {
+        amount * PRICE
+    }
+}
 
 // Don't modify this function!
 #[test]
